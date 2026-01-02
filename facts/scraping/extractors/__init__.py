f9450892b421eae87135ec6f205a886b2dadf97e
javierlnmn <jlm03123@gmail.com>
2026-01-02 12:24:29 +0100

    Add new extractor

--- facts/scraping/extractors/__init__.py
+++ facts/scraping/extractors/__init__.py
@@ -1,4 +1,9 @@
 from .base import BaseExtractor
 from .sciencefocus_121_facts import ScienceFocus121FactsExtractor
+from .today_interesting_facts_adults import TodayInterestingFactsAdultsExtractor
 
-__all__ = ["Extractor", "ScienceFocus121FactsExtractor"]
+__all__ = [
+    "BaseExtractor",
+    "ScienceFocus121FactsExtractor",
+    "TodayInterestingFactsAdultsExtractor",
+]
---
+++ facts/scraping/extractors/today_interesting_facts_adults.py
@@ -0,0 +1,60 @@
+import logging
+
+import requests
+from bs4 import BeautifulSoup, SoupStrainer, Tag
+from django.utils.text import slugify
+
+from facts.scraping.extractors import BaseExtractor
+from facts.scraping.formatters import BaseFactFormatter, DefaultFactFormatter
+from facts.scraping.types import Fact as FactType
+
+logger = logging.getLogger(__name__)
+
+
+class TodayInterestingFactsAdultsExtractor(BaseExtractor):
+    url = "https://www.today.com/life/inspiration/interesting-facts-rcna130243"
+
+    def __init__(self, formatter: BaseFactFormatter = DefaultFactFormatter()) -> None:
+        self.formatter = formatter
+
+    def _fetch(self) -> str:
+        logger.info(f"Fetching {self.url}")
+        response = requests.get(self.url)
+        response.raise_for_status()
+        logger.info(f"Fetched {self.url} successfully")
+        return response.text
+
+    def _process_html(self, html: str) -> list[FactType]:
+        logger.info(f"Processing HTML for {self.url}")
+
+        facts_ul = SoupStrainer(
+            "ul", attrs={"class": "break-above body-ul body-list-el"}
+        )
+
+        soup = BeautifulSoup(html, "html.parser", parse_only=facts_ul)
+
+        facts: list[FactType] = []
+        for index, fact_html in enumerate(soup.find_all("li")):
+            logger.info(f"Processing fact #{index}")
+            facts.append(self._process_fact(fact_html))
+
+        return facts
+
+    def _process_fact(self, fact_html: Tag) -> FactType:
+        fact_text = fact_html.get_text(strip=True)
+
+        for tag in fact_html.find_all(True):
+            if tag.name not in ("sup", "sub"):
+                tag.unwrap()
+
+        desc_html = ""
+
+        fact_text, desc_html = self.formatter.format(fact_text, desc_html)
+
+        identifier = slugify(fact_text)
+
+        return FactType(fact=fact_text, identifier=identifier, description=desc_html)
+
+    def run(self) -> list[FactType]:
+        html = self._fetch()
+        return self._process_html(html)
