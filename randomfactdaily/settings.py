bba78287adcb66e0e0b6a4488b6a5578fc3c24cb
javierlnmn <jlm03123@gmail.com>
2026-01-27 12:02:02 +0100

    Fix DB persistance issue

--- randomfactdaily/settings.py
+++ randomfactdaily/settings.py
@@ -111,7 +111,7 @@ LOGGING = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db/db.sqlite3",
     }
 }
 
