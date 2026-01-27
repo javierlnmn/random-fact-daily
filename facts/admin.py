eeadc42deb3441c95c58d11512176551d445c970
javierlnmn <jlm03123@gmail.com>
2026-01-27 10:44:37 +0100

    Avoid repeated facts by adding status and visited date

--- facts/admin.py
+++ facts/admin.py
@@ -1,15 +1,53 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-from .models import Category, Fact
+from .models import Category, Fact, FactStatus
+
+status_color = {
+    FactStatus.VISITED: {
+        "color": "#41d97b",
+        "background-color": "rgba(65, 217, 123, 0.2)",
+    },
+    FactStatus.CURRENT: {
+        "color": "#417bd9",
+        "background-color": "rgba(65, 123, 217, 0.2)",
+    },
+    FactStatus.NOT_VISITED: {
+        "color": "#d94164",
+        "background-color": "rgba(217, 65, 100, 0.2)",
+    },
+}
 
 
 @admin.register(Fact)
 class FactAdmin(admin.ModelAdmin):
-    list_display = ("fact", "identifier", "created_at", "updated_at")
-    list_filter = ("created_at", "updated_at")
+    list_display = ("fact", "status_display", "identifier", "created_at", "updated_at")
+    list_filter = ("status", "created_at", "updated_at")
     search_fields = ("fact", "identifier", "description")
     list_display_links = ("fact", "identifier")
     prepopulated_fields = {"identifier": ("fact",)}
+
+    def status_display(self, obj):
+        status = FactStatus(obj.status)
+        return format_html(
+            f"""<span
+                    style='
+                        display: inline-block;
+                        color: {status_color[status]["color"]};
+                        background-color: {status_color[status]["background-color"]};
+                        padding: 2px 4px;
+                        border-radius: 50px;
+                        min-width: 100px;
+                        text-align: center;
+                    '>
+                    {status.label}
+                </span>
+            """,
+        )
+
+    status_display.allow_tags = True
+    status_display.admin_order_field = "status"
+    status_display.short_description = "status"
 
 
 admin.site.register(Category)
--- facts/models.py
+++ facts/models.py
@@ -2,6 +2,7 @@ import datetime
 import random
 
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -14,25 +15,84 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class FactStatus(models.TextChoices):
+    VISITED = "visited", "Visited"
+    CURRENT = "current", "Current"
+    NOT_VISITED = "not_visited", "Not Visited"
+
 
 class Fact(models.Model):
     identifier = models.SlugField(max_length=255, unique=True)
     fact = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField(Category, related_name="facts")
+    categories = models.ManyToManyField(Category, blank=True, related_name="facts")
+    status = models.CharField(
+        max_length=255, choices=FactStatus, default=FactStatus.NOT_VISITED
+    )
+    date_visited = models.DateField(blank=True, null=True, default=None)
 
     def __str__(self):
         return self.fact
 
+    def update_status(self, status: FactStatus):
+        self.status = status
+        if status == FactStatus.CURRENT:
+            self.date_visited = timezone.now().date()
+        elif status == FactStatus.NOT_VISITED:
+            self.date_visited = None
+        self.save()
+
+    @classmethod
+    def reset_all_facts(cls):
+        cls.objects.all().update(status=FactStatus.NOT_VISITED, date_visited=None)
+
+    @classmethod
+    def update_old_visited_facts(cls, date=timezone.now()):
+        today = timezone.now().date()
+        cls.objects.filter(
+            status=FactStatus.CURRENT,
+            date_visited__lt=today,
+        ).update(
+            status=FactStatus.VISITED,
+        )
+
+    @classmethod
+    def get_today_current_fact(cls):
+        today = timezone.now().date()
+        try:
+            return cls.objects.get(status=FactStatus.CURRENT, date_visited=today)
+        except cls.DoesNotExist:
+            return None
+        except cls.MultipleObjectsReturned:
+            return None
+
     @classmethod
     def get_fact_from_date(cls, date: datetime.date):
+        today_current_fact = cls.get_today_current_fact()
+        if today_current_fact is not None:
+            return today_current_fact
+
+        cls.update_old_visited_facts(date)
+
+        facts = cls.objects.filter(status=FactStatus.NOT_VISITED)
+
         num_date = date.toordinal()
-        fact_count = cls.objects.count()
+        fact_count = facts.count()
+
         if fact_count == 0:
-            return None
+            cls.reset_all_facts()
+            facts = cls.objects.filter(status=FactStatus.NOT_VISITED)
+
         rng = random.Random(num_date)
         choice = rng.choice(range(fact_count))
-        fact = cls.objects.all()[choice]
+        fact = facts[choice]
+
+        fact.update_status(FactStatus.CURRENT)
+
         return fact
